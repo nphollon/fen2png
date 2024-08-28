d1d16f7c6d494c325c97fba9342c768e5649a5e1
Nick Hollon <falling.masonry@gmail.com>
2024-08-27 21:48:41 -0700

    Creating a canvas, exporting to data URL

--- fen2png.js
+++ fen2png.js
@@ -1,1 +1,18 @@
-console.log("...");
+const canvas = document.getElementById("board-canvas");
+canvas.width = 1000;
+canvas.height = 1000;
+
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 150, 100);
+
+const img = document.getElementById("bB");
+
+img.addEventListener("load", () => {
+  ctx.drawImage(img, 0, 0, 50, 50);
+  console.log(canvas.toDataURL());
+});
+
+
+console.log(canvas.toDataURL());
